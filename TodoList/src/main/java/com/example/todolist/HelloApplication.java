9cd68cb10a4644f1217fc21b0b333e799eb3c313
TheVSNA <nicolavisona1@gmail.com>
2022-06-25 22:36:24 +0200

    Add todo element save and visualization

--- TodoList/src/main/java/com/example/todolist/HelloApplication.java
+++ TodoList/src/main/java/com/example/todolist/HelloApplication.java
@@ -7,12 +7,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.time.LocalDate;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
@@ -30,13 +33,13 @@ public class HelloApplication extends Application
         openView.setFitHeight(40);
 
 
-        MenuItem test = new MenuItem("Aggiungi elemento");   //create submenu
-        test.setOnAction(addTodo());
-        test.setStyle("-fx-font-size: 20px");   //-fx-background-color: #fffdd0
+        MenuItem menuItem1 = new MenuItem("Aggiungi elemento");   //create submenu
+        menuItem1.setOnAction(addTodo());
+        menuItem1.setStyle("-fx-font-size: 20px");   //-fx-background-color: #fffdd0
 
         Menu mainmenu = new Menu(""); //create menu
         mainmenu.setGraphic(openView);    //set icon
-        mainmenu.getItems().add(test);
+        mainmenu.getItems().add(menuItem1);
         //mainmenu.setStyle("-fx-background-color: #fffdd0");
 
         MenuBar menuBar = new MenuBar();    //create menu bar
@@ -45,10 +48,44 @@ public class HelloApplication extends Application
         //menuBar.setStyle("-fx-background-color: #fffdd0");
 
         VBox root = new VBox(menuBar);
-        GridPane grid = new GridPane();
+        BorderPane grid = new BorderPane();
         root.getChildren().add(grid);
 
         Scene scene = new Scene(root, 800, 800);
+
+
+
+        VBox center = new VBox();
+        SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd");
+        String todaysdate = dt1.format(new Date()).toString();  //TODO LET USER CHOOSE DATE
+        try {
+            BufferedReader bufferreader = new BufferedReader(new FileReader("todos.txt"));
+            String line;
+            while ((line = bufferreader.readLine()) != null) {
+                String date = line.substring(0,line.indexOf(';'));
+                System.out.println(date.toString());
+                if(date.equals(todaysdate)){
+                    HBox todoelement = new HBox();
+                    CheckBox checkBox = new CheckBox();
+                    Label todo = new Label(line.substring(line.lastIndexOf(';')));
+                    todoelement.getChildren().addAll(checkBox,todo);
+                    center.getChildren().add(todoelement);
+                }
+            }
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        grid.setCenter(center);
+
+        VBox left = new VBox(); //TODO IMPLEMENT
+        Label test = new Label("test");
+        left.getChildren().add(test);
+
+        grid.setLeft(left);
+
 
         primaryStage.setTitle("TodoList");
         primaryStage.setScene(scene);
@@ -82,7 +119,7 @@ public class HelloApplication extends Application
                 todoText.setMaxWidth(400);
 
                 Button confirm = new Button("Conferma");
-                confirm.setOnAction(confirmAddTodo(todoDate.getValue(),todoTags.getText(),todoText.getText()));
+                confirm.setOnAction(confirmAddTodo(todoDate,todoTags,todoText,stage));
 
                 error= new Label();
                 vbox.getChildren().addAll(hbox,todoText,confirm,error);
@@ -91,15 +128,26 @@ public class HelloApplication extends Application
             }
         };
     }
-    public EventHandler<ActionEvent> confirmAddTodo(LocalDate date, String tags, String todo) {
+    public EventHandler<ActionEvent> confirmAddTodo(DatePicker date, TextField tags, TextArea todo, Stage stage) {
+
         return new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-                if(date==null || todo ==null){
+                System.out.println(""+date.getValue().toString()+" "+tags.getText()+" "+todo.getText());
+                if(date == null || todo == null){
                     error.setText("Errore nell'inserimento della data o del testo"); //add window error instead
                 }else{
                     //save element
+                    try {
+                        FileWriter myWriter = new FileWriter("todos.txt",true);
+                        myWriter.write(""+date.getValue().toString()+ ";"+tags.getText()+";"+todo.getText()+"\r\n");
+                        myWriter.close();
+
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
                 }
+                stage.close();
             }
         };
     }
